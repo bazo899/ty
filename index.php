74bb1f8adfdaaa210f7ca85dc99fc70894cbc0ac
krd <107356857+bazo899@users.noreply.github.com>
2022-08-03 18:30:31 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+
+
+
+<form method="POST" action="welcome.php">    
+    <input name="user" type="email">
+    <input type="password" name="pass"><b>
+    <input type="submit" value="submit" name="submit">
+      
+</form>
